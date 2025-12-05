6aedc27b90c82b920ef8e9d21267b58b503b4078
marino5710 <brianmarin4@gmail.com>
2025-12-04 22:06:23 -0600

    c

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -306,10 +306,7 @@
                                     Fecha</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                                    Cliente</th>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                                    Empresa</th>
+                                    Cliente/Empresa</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
                                     Vendedor</th>
