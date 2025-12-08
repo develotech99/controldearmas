e17851a88b2fe3a86f6313f4a82f762aaf381b16
marino5710 <brianmarin4@gmail.com>
2025-12-08 11:58:00 -0600

    change

--- app/Http/Controllers/DeudasController.php
+++ app/Http/Controllers/DeudasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon; 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
